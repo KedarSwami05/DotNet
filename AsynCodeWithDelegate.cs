20bc2b383f4e7381899da0227a08581e8f8431ec
Kedar Swami <kmswami005@gmail.com>
2025-05-31 12:38:53 +0530

    Add files via upload

---
+++ AsynCodeWithDelegate.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+//call a fun asynchrously
+//no para and void return type
+
+namespace AsynCodeWithDelegate1
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Before ");
+            Action oDel = Display;
+            //oDel(); //sync call
+            oDel.BeginInvoke(null, null); //async call fun on separate thread
+            Console.WriteLine("After ");
+            Console.ReadLine();
+        }
+
+        static void Display()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("Display");
+        }
+    }
+}
+
+
+namespace AsynCodeWithDelegate2
+{
+    internal class Program
+    {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("Before ");
+            Action<string> oDel = Display;
+            //oDel(); //sync call
+            oDel.BeginInvoke("Passed value",null, null); //async call fun on separate thread
+            Console.WriteLine("After ");
+            Console.ReadLine();
+        }
+
+        static void Display(string s)
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("Display" +s);
+        }
+    }
+}
+
+namespace AsynCodeWithDelegate3
+{
+    internal class Program
+    {
+        static void Main3(string[] args)
+        {
+            Console.WriteLine("Before ");
+            Func<string,string> oDel = Display;
+            //oDel(); //sync call
+            oDel.BeginInvoke("Passed value", new AsyncCallback(CallbackFun), null); //async call fun on separate thread
+            Console.WriteLine("After ");
+            Console.ReadLine();
+        }
+
+        static void CallbackFun(IAsyncResult ar)
+        {
+            Console.WriteLine("callback fun called");
+        }
+        static string Display(string s)
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("Display" + s);
+            return s.ToUpper();
+        }
+    }
+}
+
+
+namespace AsynCodeWithDelegate4
+{
+    internal class Program
+    {
+        static void Main4(string[] args)
+        {
+            Console.WriteLine("Before ");
+            Func<string, string> oDel = Display;
+            //oDel(); //sync call
+            IAsyncResult ar1 = oDel.BeginInvoke("Passed value", delegate(IAsyncResult ar)
+        {
+                Console.WriteLine("callback fun called");
+                string retval = oDel.EndInvoke(ar);
+                Console.WriteLine(retval);
+            }, null); //async call fun on separate thread
+            Console.WriteLine("After ");
+            Console.ReadLine();
+        }
+
+        //static void CallbackFun(IAsyncResult ar)
+        //{
+        //    Console.WriteLine("callback fun called");
+        //    string retval = oDel.EndInvoke(ar); 
+        //    Console.WriteLine(retval);
+        //}
+        static string Display(string s)
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("Display" + s);
+            return s.ToUpper();
+        }
+    }
+}
+
+namespace AsynCodeWithDelegate5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before ");
+            Func<string, string> oDel = Display;
+            //oDel(); //sync call
+            //oDel.BeginInvoke("Passed value", new AsyncCallback(CallbackFun), "extra data");
+            //oDel.BeginInvoke("Passed value", new AsyncCallback(CallbackFun), 123);//async call fun on separate thread
+            oDel.BeginInvoke("Passed value", new AsyncCallback(CallbackFun), oDel);
+            Console.WriteLine("After ");
+            Console.ReadLine();
+        }
+
+        static void CallbackFun(IAsyncResult ar)
+        {
+            //string last_para = ar.AsyncState.ToString();
+            //Console.WriteLine(last_para);
+
+            //int last_para = (int)ar.AsyncState;
+            //Console.WriteLine(last_para);
+
+            Func<string, string> oDel =(Func<string, string>) ar.AsyncState;
+
+            Console.WriteLine("callback fun called");
+            string retval = oDel.EndInvoke(ar);
+            Console.WriteLine(retval);
+
+        }
+        static string Display(string s)
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("Display" + s);
+            return s.ToUpper();
+        }
+    }
+}
---
+++ WorkingWithTasks.cs
@@ -0,0 +1,177 @@
+﻿namespace WorkingWithTasks
+{
+    internal class Program
+    {
+        static void Main1()
+        {
+            Task t1 = new Task(Fun1);
+            Task t2 = new Task(Fun2);
+            t1.Start();
+            t2.Start();
+            //t1.Wait();
+            Console.ReadLine();
+        }
+
+         static void Fun1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("First :" + i);
+            }
+        }
+
+        static void Fun2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Second fun called from  {0},{1}", Thread.CurrentThread.ManagedThreadId, i);
+            }
+        }
+    }
+}
+
+namespace Example2
+{
+    internal class Program
+    {
+        static void Main2()
+        {
+            Task t1 =  Task.Run(Fun1);              // dont need to start task it start automatically
+            Task t2 =  Task.Factory.StartNew(Fun2);
+            
+            //t1.Wait();
+            Console.ReadLine();
+        }
+
+        static void Fun1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("First :" + i);
+            }
+        }
+
+        static void Fun2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Second fun called from  {0},{1}", Thread.CurrentThread.ManagedThreadId, i);
+            }
+        }
+    }
+}
+
+
+namespace Example3
+{
+    internal class Program1
+    {
+        static void Main3()
+        {
+            Task t1 = new Task(Fun1, "Passed value");              // dont need to start task it start automatically
+            Task t2 = Task.Factory.StartNew(Fun2, "aaaaaaaa");
+
+            string s = "ccccc";
+
+            Task.Run(delegate () { Console.WriteLine(s); });
+            Task.Run(() => { Console.WriteLine(s); });
+            //t1.Wait();
+            Console.ReadLine();
+        }
+
+
+        static void Fun1(Object obj)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("First :" + i);
+            }
+        }
+
+        static void Fun2(Object obj)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Second fun called from  {0},{1}", Thread.CurrentThread.ManagedThreadId, i);
+            }
+        }
+    }
+}
+
+namespace Example4
+{
+    internal class Program1
+    {
+        static void Main4()
+        {
+            Task <int> t1 = new Task<int>(Fun1,"passed value");              // dont need to start task it start automatically
+            t1.Start();
+            Console.WriteLine("Code before result ");
+
+            Console.WriteLine(t1.Result);
+            Console.ReadLine();
+        }
+
+
+        static int Fun1(Object obj)
+        {
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("First :" + i);
+            }
+            return i;
+        }
+
+        static void Fun2(Object obj)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Second fun called from  {0},{1}", Thread.CurrentThread.ManagedThreadId, i);
+            }
+        }
+    }
+}
+
+namespace Example5
+{
+    internal class Program1
+    {
+        static void Main()
+        {
+            Task<int> t1 = new Task<int>(Fun2, "passed value t2");              // dont need to start task it start automatically
+            t1.Start();
+            Task<int> t3 = new Task<int>(Fun2, "passed value t3");
+        
+
+            Console.WriteLine(t1.Result); //waiting call
+            Console.WriteLine(t3.Result);
+            Console.ReadLine();
+        }
+
+
+        static int Fun1(Object obj)
+        {
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("First :" + i);
+            }
+            return i;
+        }
+
+        static int Fun2(Object obj)
+        {
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Second fun called from  {0},{1}", i, obj.ToString());
+            }
+            return i;
+        }
+    }
+}
+
+
+
+
